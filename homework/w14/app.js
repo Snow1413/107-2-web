05353e9f916ef322aef3aaf0b07dde71b3c32865
Snow1413 <z29401413@gms.tku.edu.tw>
2019-05-15 03:11:59 +0800

    hw-w14

--- homework/w14/app.js
+++ homework/w14/app.js
@@ -1,33 +1,67 @@
 document.getElementById('loan-form').addEventListener('submit', function (e) {
     document.getElementById('loading').style.display = "block";
     document.getElementById('results').style.display = "none";
-    setTimeout(calculateResults, 2000);
+    setTimeout(calculateResults, 500);
     e.preventDefault();
 });
 
 function calculateResults() {
     console.log('calculating ...');
+    let height = document.getElementById('height').value;
+    let weight = document.getElementById('weight').value;
+
+    height /= 100;
+    let BMI = (weight / (height * height)).toFixed(1);
+
+
+    console.log(height);
+    console.log(weight);
+    console.log(BMI);
+
+
+    if (BMI < 18.5) {
+        gap = (18.5 - BMI).toFixed(1);
+        showAlert(`您的BMI為${BMI}，距離標準的BMI還差${gap}`);
+    } else if (BMI > 24) {
+        gap = (BMI - 24).toFixed(1);
+        showAlert(`您的BMI為${BMI}，距離標準的BMI還差${gap}`);
+    } else {
+        showAlert(`您的BMI為${BMI}，已在標準的範圍內`);
+    }
+
+    function showAlert(msg) {
+        console.log(document.querySelector('#results p h5'))
+        document.querySelector('#results p').style.display = "block";
+        document.querySelector('#results p').innerHTML = `<h5>${msg}</h5>`;
+    }
+
+
+
+
+
+
+    let p = document.querySelectorAll("tr");
+
+    p.forEach(function (e) {
+        e.className = '';
+    });
+
+    if (BMI < 18.5) {
+        p[1].className = 'bg-secondary';
+    } else if (BMI < 24) {
+        p[2].className = 'bg-secondary';
+    } else if (BMI < 27) {
+        p[3].className = 'bg-secondary';
+    } else if (BMI < 30) {
+        p[4].className = 'bg-secondary';
+    } else if (BMI < 35) {
+        p[5].className = 'bg-secondary';
+    } else {
+        p[6].className = 'bg-secondary';
+    }
+
 
     // UI Vars
-    const amount = document.getElementById('amount');
-    const interest = document.getElementById('interest');
-    const years = document.getElementById('years');
-    const monthPayment = document.getElementById('monthly-payment');
-    const totalPayment = document.getElementById('total-payment');
-    const totalInterest = document.getElementById('total-interest');
-
-    const principal = parseFloat(amount.value);
-    const calculateInterest = parseFloat(interest.value) / 100 / 12;
-    const calculatePayments = parseFloat(years.value) * 12;
-
-    const x = Math.pow(1 + calculateInterest, calculatePayments);
-    const monthly = (principal * calculateInterest * x) / (x - 1);
-
-    if (isFinite(monthly)) {
-        monthPayment.value = monthly.toFixed(2);
-        totalPayment.value = (monthly * calculatePayments).toFixed(2);
-        totalInterest.value = ((monthly * calculatePayments) - principal).toFixed(2);
-    }
     document.getElementById('loading').style.display = "none";
     document.getElementById('results').style.display = "block";
 }
